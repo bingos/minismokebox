b6d2e31419d07ad6eb0a988e0bbe29141d62eca7
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-02-17 11:20:09 +0000

    Match only on . in Makefile.PL

--- lib/App/SmokeBox/Mini.pm
+++ lib/App/SmokeBox/Mini.pm
@@ -26,7 +26,7 @@ use vars qw($VERSION);
 
 use constant CPANURL => 'ftp://cpan.cpantesters.org/CPAN/';
 
-$VERSION = '0.62';
+$VERSION = '0.64';
 
 $ENV{PERL5_MINISMOKEBOX} = $VERSION;
 
--- lib/App/SmokeBox/Mini/Plugin.pm
+++ lib/App/SmokeBox/Mini/Plugin.pm
@@ -3,7 +3,7 @@ package App::SmokeBox::Mini::Plugin;
 use strict;
 use warnings;
 
-our $VERSION = '0.62';
+our $VERSION = '0.64';
 
 qq[Smokin' plugins];
 
